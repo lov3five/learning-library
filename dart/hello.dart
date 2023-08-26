9b999bd50926ab821d56711a9a2fefe56255368e
lov3five <luongtranstaff@gmail.com>
2023-08-26 15:36:40 +0700

    Learning Dart

--- dart/hello.dart
+++ dart/hello.dart
@@ -1,8 +1,39 @@
 import 'dart:io';
+
+
+class Person {
+  String name;
+  int namSinh;
+  Person(this.name, this.namSinh) {
+
+  }
+
+  void tinhTuoi() {
+    int namHienTai = 2023;
+    if (namSinh != null) {
+      int tuoi = namHienTai - namSinh;
+      print("Tuổi của $name hiện tại: $tuoi tuổi");
+    } else {
+      print("$name chưa được nhập năm sinh!!!");
+    }
+    
+
+  }
+  
+}
+
 void main() {
   var firstName = 'Tran';
   var lastName = 'Thanh Luong';
   var age = 22;
   var sex = 'Male';
   var school = 'Industrial University of Ho Chi Minh City';
+  var favouriteColor = ['Blue', 'Silver'];
+  var socialLink = {
+    'facebook': 'http://facebook.com/l-3012',
+    'instagram': 'http://facebook.com/l-3012'
+  };
+
+  Person Luong = Person('Luong', 2001);
+  Luong.tinhTuoi();
 }
