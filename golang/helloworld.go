8a1a74264964e86c7a90f0732e076409c88b208a
lov3five <luongtranstaff@gmail.com>
2023-08-30 16:51:02 +0700

    Learning go

--- golang/helloworld.go
+++ golang/helloworld.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
+	// Output: Hello world
 	fmt.Println("Hello world")
+	/*
+	From Naker with ❤
+	*/
+	fmt.Print("I like Golang because it is very fast and high performance")
 }
