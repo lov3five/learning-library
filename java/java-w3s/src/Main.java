c828768307f12305963f321078cfdcd0b8ce4e72
lov3five <luongtranstaff@gmail.com>
2024-01-05 16:23:55 +0700

    Overview Java with W3S

---
+++ java/java-w3s/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello world, I'm NakerVN";
+        System.out.println("This is string " + str);
+
+        int num = 45;
+        System.out.println("This is number " + num);
+
+        float floatNumber = 3.14f;
+        System.out.println("This is float " + floatNumber);
+
+        char ch = 65; // ASCII Table Reference (https://www.w3schools.com/charsets/ref_html_ascii.asp)
+        System.out.println("This is character " + ch);
+    }
+}
